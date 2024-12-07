7bb8b2255103cffcf4f1e4e255c86c1e972f32f5
Aswath Shakthi <aswathshakthi@outlook.com>
2024-12-07 17:12:57 +0530

    Update myapp.py

--- src/myapp.py
+++ src/myapp.py
@@ -2,9 +2,10 @@ import requests
 import streamlit as st
 from server import port_no
 import subprocess
-server_process = subprocess.Popen(["python", "server.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 from pathlib import Path
 import atexit
+
+server_process = subprocess.Popen(["python", "server.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 # Add a logo in the sidebar
@@ -25,6 +26,7 @@ if st.button("Analyze"):
         response = requests.post(API_URL, json = {"query": query})
         if response.status_code == 200:
             result = response.text
+            st.write("Model runs on Multinomial Naive Bayes, trained on 4 Million Amazon Product and Movie reviews with 78% accuracy")
             output_box = st.text_area("Sentiment",result)
         else:
             st.error("Error calling API")
