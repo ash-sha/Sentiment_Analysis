826a12f1ebc4bbca21e492489dd2d4e98a8b4262
Aswath Shakthi <aswathshakthi@outlook.com>
2024-12-07 21:53:50 +0530

    Update app_str.py

--- src/app_str.py
+++ src/app_str.py
@@ -1,12 +1,12 @@
-
+import os
+import pickle
 import streamlit as st
 from pathlib import Path
-from inference import predict_polarity
 import nltk
-import os 
+
+from data_processing import preprocess_text_nltk
 
 nltk.download('stopwords')
-MODEL_SAVE_PATH = os.getenv('MODEL_SAVE_PATH', 'Sentiment_Analysis/models/sample_trained_model.pickle')
 
 # Add a logo in the sidebar
 logo_path = Path(__file__).parent / "pic.jpeg"
@@ -16,13 +16,26 @@ st.title("Sentiment Analysis Tool")
 st.write("""
 ##### Analyze the sentiment of your text in real-time.
 """)
+# Function to load the model and vectorizer, and make predictions
+def predict_polarity(user_query: str) -> str:
+    saved_model_dic = pickle.load(open("/Users/aswathshakthi/PycharmProjects/MLOps/Sentiment_Analysis/models/sample_trained_model.pickle", "rb"))
+    saved_clf = saved_model_dic['model']
+    saved_vectorizer = saved_model_dic['vectorizer']
 
+    # Preprocess the user query
+    preprocessed_query = preprocess_text_nltk([user_query])
 
+    # Transform the query text using the saved vectorizer
+    query_vec = saved_vectorizer.transform(preprocessed_query)
+
+    # Predict the polarity
+    prediction = saved_clf.predict(query_vec)
+
+    return prediction[0]
 query = st.text_input("Enter your Sentence:")
 
 if st.button("Analyze"):
     if query:
-
         result = predict_polarity(query)
         st.write("Model runs on Multinomial Naive Bayes, trained on 4 Million Amazon Product and Movie reviews with 78% accuracy")
         output_box = st.text_area("Sentiment",result)
