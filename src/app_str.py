fc0a90cf2a1dba369a415ba169b86886c64c2aba
Aswath Shakthi <aswathshakthi@outlook.com>
2024-12-07 21:33:33 +0530

    Create app_str.py

---
+++ src/app_str.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+from pathlib import Path
+from inference import predict_polarity
+
+
+# Add a logo in the sidebar
+logo_path = Path(__file__).parent / "pic.jpeg"
+st.sidebar.image(str(logo_path), use_container_width=True)
+
+st.title("Sentiment Analysis Tool")
+st.write("""
+##### Analyze the sentiment of your text in real-time.
+""")
+
+
+query = st.text_input("Enter your Sentence:")
+
+if st.button("Analyze"):
+    if query:
+
+        result = predict_polarity(query)
+        st.write("Model runs on Multinomial Naive Bayes, trained on 4 Million Amazon Product and Movie reviews with 78% accuracy")
+        output_box = st.text_area("Sentiment",result)
+
+    else:
+        st.warning("Please enter your query")
+
