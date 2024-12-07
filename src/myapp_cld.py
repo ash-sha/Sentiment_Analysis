ba0a280a3bd30ed1b7340026f66e58f4a4703da7
Aswath Shakthi <aswathshakthi@outlook.com>
2024-12-08 00:35:41 +0530

    Create myapp_cld.py

---
+++ src/myapp_cld.py
@@ -0,0 +1,33 @@
+# for cloud access
+
+import requests
+import streamlit as st
+from pathlib import Path
+
+
+# Add a logo in the sidebar
+logo_path = Path(__file__).parent / "pic.jpeg"
+st.sidebar.image(str(logo_path), use_container_width=True)
+
+st.title("Sentiment Analysis Tool")
+st.write("""
+##### Analyze the sentiment of your text in real-time.
+""")
+
+API_URL = "https://sentiment-analysis-3m74.onrender.com/predict"
+
+query = st.text_input("Enter your Sentence:")
+
+if st.button("Analyze"):
+    if query:
+        response = requests.post(API_URL, json = {"query": query})
+        if response.status_code == 200:
+            result = response.text
+            st.write("Model runs on Multinomial Naive Bayes, trained on 4 Million Amazon Product and Movie reviews with 78% accuracy")
+            output_box = st.text_area("Sentiment",result)
+        else:
+            st.error("Error calling API")
+    else:
+        st.warning("Please enter your query")
+
+
